8927753086ac28320f6a8955d1640b0fbb565ec8
Vivian Wang <ywang7@babson.edu>
2021-09-24 16:00:59 -0400

    V1.0

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ from flask import Flask, render_template
 # Configure application
 app = Flask(__name__)
 
-@app.route('/index', methods=['POST', 'GET'])
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/')
 def index():
+    return render_template('index.html')
 
-    greeting = 'Welcome to My Data Science Portfolio Website'
 
-    return render_template('/index.html',
-                            greeting=greeting)
+
+if __name__ == '__main__':
+    app.run(debug=True)
